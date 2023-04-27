a3946628cd2732bd4e4633cb062f831a73e47e9e
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-04-27 16:36:10 -0300

    ok

---
+++ background.js
@@ -0,0 +1,29 @@
+function script(conteudo) {
+  var linhas = document.querySelector("#form > fieldset > table").rows.length;
+  var i = 1;
+  var resultado = document.querySelector("#form > fieldset > table").rows[i].cells[2].innerText + "<br>";
+  i++;
+  while (i < linhas) {
+    resultado += document.querySelector("#form > fieldset > table").rows[i].cells[2].innerText + "<br>";
+    i++;
+  }
+  // console.log(conteudo);
+  // conteudo.value = resultado;    
+  // window.open("", resultado);
+  newpopupWindow = window.open('', 'pagina', "width=400 height=400");
+  newpopupWindow.document.write(resultado);
+  // return resultado;
+  // document.querySelector("#conteudo_extension").value =  resultado;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#btn1").addEventListener("click", function () {
+    (async () => {
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: script
+      });
+    })()
+  });
+});   
