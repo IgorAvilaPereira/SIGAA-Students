3d8a443f0568f29aa919a019631a19efba7e4b13
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-09-17 09:22:48 -0300

    ok

--- background.js
+++ background.js
@@ -1,54 +1,58 @@
 function script2(conteudo) {
   var tabela = document.querySelector(".listing");
-  var linhas = tabela.rows; 
+  var linhas = tabela.rows;
   var primeiraColuna = [];
   var segundaColuna = [];
   for (let i = 1; i < linhas.length; i++) {
-    var celula = linhas[i].cells[0];             
+    var celula = linhas[i].cells[0];
     if (celula && celula.style.fontWeight == "bold") {
-      primeiraColuna.push(celula.innerText.trim());    
-    }        
-  } 
+      primeiraColuna.push(celula.innerText.trim());
+    }
+  }
   var linhas = document.querySelectorAll('[style="width:150px;text-align:center;"]');
-  for (let i = 0; i < linhas.length; i++) {        
-      segundaColuna.push(linhas[i].innerText);    
-  }  
-  if (primeiraColuna.length > 0) { 
+  for (let i = 0; i < linhas.length; i++) {
+    segundaColuna.push(linhas[i].innerText);
+  }
+  if (primeiraColuna.length > 0) {
     newpopupWindow = window.open('', 'pagina', "");
     newpopupWindow.document.write("");
-    var output = document.getElementById("linkNomeTurma").innerText+" <br> <br>";
+    var output = document.getElementById("linkNomeTurma").innerText + " <br> <br>";
     for (let index = 0; index < primeiraColuna.length; index++) {
-      var element1 = primeiraColuna[index];            
-      var element2 = segundaColuna[index];        
-      output+= "* "+element1+" - "+element2+"<br>";                  
+      var element1 = primeiraColuna[index];
+      var element2 = segundaColuna[index];
+      output += "* " + element1 + " - " + element2 + "<br>";
     }
     newpopupWindow.document.write(output);
+  } else {
+    newpopupWindow = window.open('', 'pagina', "");
+    newpopupWindow.document.write("");
+    newpopupWindow.document.write(document.getElementById("linkNomeTurma").innerText + " <br> <br>");
   }
 }
 
 function script1(conteudo) {
   try {
-     var linhas = document.querySelector("#form > fieldset > table").rows.length;
-      var i = 1;
-      var resultado = "<br><br>================<br><br>";
-      while (i < linhas) {
-        resultado += document.querySelector("#form > fieldset > table").rows[i].cells[2].innerText + "<br><br>================<br><br>";
-        i++;
-      }
-      if (linhas > 0) {
-        //  baixando todos os trabalhos!
-        document.querySelector("#form > fieldset > p > a").click();
+    var linhas = document.querySelector("#form > fieldset > table").rows.length;
+    var i = 1;
+    var resultado = "<br><br>================<br><br>";
+    while (i < linhas) {
+      resultado += document.querySelector("#form > fieldset > table").rows[i].cells[2].innerText + "<br><br>================<br><br>";
+      i++;
+    }
+    if (linhas > 0) {
+      //  baixando todos os trabalhos!
+      document.querySelector("#form > fieldset > p > a").click();
 
-        newpopupWindow = window.open('', 'pagina', "");
-        newpopupWindow.document.write("");
-        newpopupWindow.document.write(document.getElementById("linkNomeTurma").innerText+"<br> <br>"+ document.querySelector("#form > fieldset > div:nth-child(3)").innerText + resultado);    
-      }
-  } catch (error) {
-     newpopupWindow = window.open('', 'pagina', "");
+      newpopupWindow = window.open('', 'pagina', "");
       newpopupWindow.document.write("");
-      newpopupWindow.document.write(document.getElementById("linkNomeTurma").innerText+"<br> <br>");     
+      newpopupWindow.document.write(document.getElementById("linkNomeTurma").innerText + "<br> <br>" + document.querySelector("#form > fieldset > div:nth-child(3)").innerText + resultado);
+    }
+  } catch (error) {
+    newpopupWindow = window.open('', 'pagina', "");
+    newpopupWindow.document.write("");
+    newpopupWindow.document.write(document.getElementById("linkNomeTurma").innerText + "<br> <br>");
   }
-     
+
 }
 
 document.addEventListener("DOMContentLoaded", function () {
